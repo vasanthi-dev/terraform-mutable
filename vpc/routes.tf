725ef24d00f0863319aef47898f1fdca410ff875
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-24 16:36:42 -0500

    VPC creation

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -16,6 +16,21 @@ resource "aws_route_table" "private-route" {
       network_interface_id       = ""
       transit_gateway_id         = ""
       vpc_endpoint_id            = ""
+    },
+    {
+      cidr_block                 = "0.0.0.0/0"
+      vpc_peering_connection_id  = ""
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      nat_gateway_id             = aws_nat_gateway.ngw.id
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
     }
   ]
 
