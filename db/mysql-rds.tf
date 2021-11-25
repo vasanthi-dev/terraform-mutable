f3b3819fd859b70eba2c0be45fbf79da5e390337
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-25 01:19:29 -0500

    VPC creation

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,29 +1,25 @@
-#resource "aws_db_instance" "mysql" {
-#  allocated_storage    = 10
-#  engine               = "mysql"
-#  engine_version       = "5.7"
-#  instance_class       = "db.t3.micro"
-#  name                 = "dummy"
-#  username             = "vadmin"
-#  password             = "vadmin"
-#  parameter_group_name = aws_db_parameter_group.pg.name
-#  skip_final_snapshot  = true
-#}
-#
-#resource "aws_db_parameter_group" "pg" {
-#  name   = "mysql-${var.ENV}-pg"
-#  family = "mysql5.7"
-#}
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "dummy"
+  username             = "vadmin"
+  password             = "vadmin"
+  parameter_group_name = aws_db_parameter_group.pg.name
+  skip_final_snapshot  = true
+}
 
-#resource "aws_db_subnet_group" "subnet-group" {
-#  name       = "main"
-#  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
-#
-#  tags = {
-#    Name = "My DB subnet group"
-#  }
-#}
+resource "aws_db_parameter_group" "pg" {
+  name   = "mysql-${var.ENV}-pg"
+  family = "mysql5.7"
+}
 
-output "PRIVATE_SUBNETS" {
-  value = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS
+resource "aws_db_subnet_group" "subnet-group" {
+  name       = "mysqldb-subnet-group-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS
+
+  tags = {
+    Name = "mysqldb-subnet-group-${var.ENV}"
+  }
 }
