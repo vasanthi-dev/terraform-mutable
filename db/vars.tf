5248e0259d1602aac6626654e81551a169ddbdfb
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-25 01:12:19 -0500

    VPC creation

--- db/vars.tf
+++ db/vars.tf
@@ -1,8 +1,1 @@
 variable "ENV" {}
-variable "VPC_CIDR_MAIN" {}
-variable "VPC_CIDR_ADDON" {}
-variable "PRIVATE_SUBNETS" {}
-variable "PUBLIC_SUBNETS" {}
-variable "AZS" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
