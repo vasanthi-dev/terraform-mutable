7d1972b7457d127ea85452e1c4dc6e6a55b018f8
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-25 14:04:47 -0500

    VPC creation

--- db/data.tf
+++ db/data.tf
@@ -12,6 +12,16 @@ data "aws_secretsmanager_secret" "secrets" {
   name = var.ENV
 }
 
-output "sec" {
-  value = data.aws_secretsmanager_secret.secrets
+data "aws_secretsmanager_secret_version" "secrets-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+#output "sec" {
+#  value = data.aws_secretsmanager_secret_version.secrets-version
+#}
+
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "echo ${jsondecode(data.aws_secretsmanager_secret_version.secrets-version)["RDS_MYSQL_USER"]} >/TMP/1"
+  }
 }
