950d471e57910804d05589b281792680721768f8
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-30 22:29:28 -0500

    VPC creation

--- db/mongodb-documentdb.tf
+++ db/mongodb-documentdb.tf
@@ -10,7 +10,7 @@ resource "aws_docdb_cluster" "docdb" {
   master_password         = local.mongo_pass
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
-  db_cluster_parameter_group_name = aws_db_parameter_group.pg.name
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.pg.name
   vpc_security_group_ids = [aws_security_group.mongodb.id]
 }
 
