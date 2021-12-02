cbd5253060c66e7c240204557981b4bac527f2c9
vasanthi-dev <vasanthi2dev@gmail.com>
2021-12-02 00:30:12 -0500

    VPC creation

---
+++ alb/data.tf
@@ -0,0 +1,23 @@
+data "terraform_remote_state" "vpc" {
+
+  backend = "s3"
+  config = {
+    bucket = "terraform-dev60"
+    key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = var.ENV
+}
+
+data "aws_secretsmanager_secret_version" "secrets-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-7-DevOps-Practice"
+  owners      = ["973714476881"]
+}
