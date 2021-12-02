66be78ea6cc955d18bf670a0fb87b5bec3588565
vasanthi-dev <vasanthi2dev@gmail.com>
2021-12-02 00:32:44 -0500

    VPC creation

--- alb/public-alb.tf
+++ alb/public-alb.tf
@@ -1,19 +1,12 @@
 resource "aws_lb" "public" {
-  name               = "roboshop-public"
+  name               = "roboshop-public-${var.ENV}"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
-
+  security_groups    = [aws_security_group.public-alb.id]
+  subnets            = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNETS_IDS
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
-
   tags = {
-    Environment = "production"
+    Environment = "roboshop-public-${var.ENV}"
   }
 }
